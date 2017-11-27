655d70272f46314e149e184f8f942057aeb948f7
ashleigharendt <32883165+ashleigharendt@users.noreply.github.com>
2017-11-27 14:29:03 +0000

    Added kindness variables, started making function
    Trying to play about with the javascript

---
+++ random_number.js
@@ -0,0 +1,18 @@
+$(document).ready(function()) {
+	$('#brain_button').click(show_kindness);
+}
+
+function show_kindness() {
+	var number = 
+
+}
+
+var kindness_name = [
+"Call a friend",
+"Leave surprise in the library"
+];
+
+var kindness_description = [
+"Calling a friend will help to relieve stress",
+"Leaving a surprise is a nice thing to do"
+]
