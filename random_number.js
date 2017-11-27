9a982404fe040916ddccf0b49e19f5c86bca464b
ashleigharendt <32883165+ashleigharendt@users.noreply.github.com>
2017-11-27 14:44:59 +0000

    Trying to make button work
    Played around with making the button alert - unsuccessful

--- random_number.js
+++ random_number.js
@@ -1,10 +1,12 @@
-$(document).ready(function()) {
-	$('#brain_button').click(show_kindness);
+$(document).ready(function() {
+	$('#brain_button').click(function{
+		alert('hello');
+	});
 });
 
 function show_kindness() {
 	var rand_no = Math.floor((Math.random() * 10) +1);
-	$('kindness_name.[rand_no]').show();
+	$(kindness_name.[rand_no]).show();
 };
 
 var kindness_name = [
