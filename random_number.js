c7d4079a6b1adaf14625cba57867ba8ceb5b9a75
ashleigharendt <32883165+ashleigharendt@users.noreply.github.com>
2017-12-06 13:57:15 +0000

    Buttons working woop woop

--- random_number.js
+++ random_number.js
@@ -1,36 +1,35 @@
 $(document).ready(function() {
 	$('#brain_button').click(show_kindness);
 	$('#smile').hide();
-    $('#new_kindness').click(new_kindness);
+    $('#new_kindness').click(show_kindness);
     $('#tell_more').hide();
     $('#new_kindness').hide();
     $('#tell_more').click(tell_more);
 });
 
 
-
 function show_kindness() {
 	var rand_no = Math.floor((Math.random() * 15));	
+    $('.RAK_Short').hide();
     $(kindnesses[rand_no]['short']).show();
     $('#store_random').data('last-random', rand_no);
-    $('#brain_button').hide();
-	$('#wobble').hide();
-	$('#smile').show();
-    $('#tell_more').show();
-    $('#new_kindness').show();
-};
-
-function new_kindness() {
-    $(kindnesses[rand_no]['short']).show();
-    $(kindnesses[rand_no]['long']).show();
-    $('.RAK_Short').hide();
-    $('.RAK_Long').hide();
-};
+    var id = this.id;
+    if (id ==('brain_button')) {
+        $('#brain_button').hide();
+        $('#wobble').hide();
+        $('#smile').show();
+        $('#tell_more').show();
+        $('#new_kindness').show();
+    }
+    else {
+        $('.RAK_Long').hide();
+    }
+    };
 
 function tell_more() {
     var rand_no = $('#store_random').data('last-random');
     $(kindnesses[rand_no]['long']).show();
-}
+};
     
 var kindnesses = [
 	{'short':'#Call_Friend',
